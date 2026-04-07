74e6a5e12288e0532ebc63baf70993d551ce753e
PatWaj88 <patrycja.wajszczyk@wp.pl>
2026-04-07 14:10:30 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
+import PasswordGate from './components/PasswordGate'
 import Dashboard from './pages/Dashboard'
 import Contracts from './pages/Contracts'
 import ContractDetail from './pages/ContractDetail'
@@ -8,16 +9,18 @@ import Settings from './pages/Settings'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/umowy" element={<Contracts />} />
-          <Route path="/umowy/:id" element={<ContractDetail />} />
-          <Route path="/przypomnienia" element={<Reminders />} />
-          <Route path="/ustawienia" element={<Settings />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
+    <PasswordGate>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/umowy" element={<Contracts />} />
+            <Route path="/umowy/:id" element={<ContractDetail />} />
+            <Route path="/przypomnienia" element={<Reminders />} />
+            <Route path="/ustawienia" element={<Settings />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
+    </PasswordGate>
   )
 }
